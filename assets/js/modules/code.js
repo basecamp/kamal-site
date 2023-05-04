a97b5d3d3521eff710cdfe6689c9b8e3dee0e980
Sean Mitchell <sean@37signals.com>
2023-05-03 17:06:22 -0700

    Add code copy buttons

---
+++ assets/js/modules/code.js
@@ -0,0 +1,36 @@
+function ready() {
+
+  var highlights = document.querySelectorAll('div.highlight, figure.highlight');
+
+  highlights.forEach(highlight => {
+
+    var button = document.createElement('button');
+
+    button.className = 'copy';
+    button.innerText = 'Copy';
+
+    highlight.append(button);
+
+    button.addEventListener('click', function(e) {
+
+      e.preventDefault();
+
+      var code = highlight.querySelector('code').innerText.trim();
+
+      window.navigator.clipboard.writeText(code);
+
+      button.innerText = 'Copied';
+
+      setTimeout(function() {
+
+        button.innerText = 'Copy';
+
+      }, 1000);
+
+    });
+
+  });
+
+}
+
+export { ready };
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,9 @@
+import * as code from './modules/code.js';
 import * as video from './modules/video.js';
 
 document.addEventListener('DOMContentLoaded', () => {
+
+  code.ready();
 
   video.ready();
 
